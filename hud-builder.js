dd62d88fec9988355e3c420ed38dbbd05fe16fa5
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-01-06 16:23:59 -0800

    Halfway through refactoring to use macros

---
+++ hud-builder.js
@@ -0,0 +1,22 @@
+var pointData = [
+    {
+        "zone": "zoneAHudText",
+        "point": "A"
+    },
+    {
+        "zone": "zoneBHudText",
+        "point": "B"
+    },
+    {
+        "zone": "zoneCHudText",
+        "point": "C"
+    }
+]
+
+var pointDict = pointData[point];
+
+var stateData = [
+    {
+        
+    }
+]
