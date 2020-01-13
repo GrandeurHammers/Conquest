99b737d34d5efab46c887181f7b93c3743d2c77a
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-01-12 20:32:10 -0800

    Optimization tools

--- triggerPointReset.js
+++ triggerPointReset.js
@@ -2,9 +2,14 @@ var result = "";
 var pointToLetter = ["A", "B", "C"];
 for (var point = 0; point < 3; point++) {
     result += `
-    destroyHudText(zone${pointToLetter[point]}HudText[0])
-    zoneControl[${point}] = null
-    zone${pointToLetter[point]}Progress = 0.016
-    wait(0.016, Wait.IGNORE_CONDITION)
-    zone${pointToLetter[point]}Progress = 0`;
+destroyHudText(zone${pointToLetter[point]}HudText[0])
+zoneControl[${point}] = null
+zone${pointToLetter[point]}Progress = []
+wait(0.016, Wait.IGNORE_CONDITION)`;
+}
+result += `
+wait(0.016, Wait.IGNORE_CONDITION)`;
+for (var point = 0; point < 3; point++) {
+    result += `
+zone${pointToLetter[point]}Progress = 0`
 }
