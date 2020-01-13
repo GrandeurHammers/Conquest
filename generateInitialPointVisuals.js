e7270de1c18ad3cc5a3469e5ffa32927eac7235a
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-01-12 18:52:21 -0800

    Initialization bug squashing and reset on Game Start

--- generateInitialPointVisuals.js
+++ generateInitialPointVisuals.js
@@ -5,7 +5,7 @@ for (var i = 0; i < indToLetter.length; i++) {
 createEffect(getAllPlayers(), Effect.RING, Color.WHITE, zoneLocations[${i}], zoneSizes[${i}], EffectReeval.VISIBILITY)
 zone${indToLetter[i]}Visuals[0] = getLastCreatedEntity()
 createInWorldText(getAllPlayers(), "${indToLetter[i]}", zoneLocations[${i}] + 2 * Vector.UP, 3, Clip.NONE, WorldTextReeval.VISIBILITY_AND_STRING, Color.WHITE, SpecVisibility.ALWAYS)
-zone${indToLetter[i]}Visuals[1] = getLastCreatedEntity()`;
+zone${indToLetter[i]}Visuals[1] = getLastCreatedText()`;
 }
 
 result;
