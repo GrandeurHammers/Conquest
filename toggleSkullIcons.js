7f9cd26754c8ac558a5cad1f2f576b5537b08faa
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-02-25 16:44:02 -0800

    Reinstate chasing

---
+++ toggleSkullIcons.js
@@ -0,0 +1,42 @@
+// Params: 
+// create (boolean) - True if creating, False if destroying
+
+var result = "";
+const numSlots = 6;
+const teamData = [
+    {
+        'color': 'WHITE',
+        'team': 'null'
+    },
+    {
+        'color': 'TEAM_1',
+        'team': 'Team.1'
+    },
+    {
+        'color': 'TEAM_2',
+        'team': 'Team.2'
+    }
+];
+function teamGenerator(team) {
+    for (var i = 0; i < numSlots; i++) {
+        result += `
+            createIcon(getAllPlayers(), getPlayersInSlot(${i}, ${teamData[team]['team']}), Icon.SKULL, IconReeval.VISIBILITY_AND_POSITION, Color.${teamData[team]['color']}, false)
+            skullIcons[${i}] = getLastCreatedEntity()`;
+    }
+}
+
+if (create) {
+    result += `
+        if zoneControl[0] == Team.1:`;
+    teamGenerator(1);
+    result += `
+        else:`;
+    teamGenerator(2);
+} else {
+    for (var i = 0; i < numSlots; i++) {
+        result += `
+        destroyIcon(skullIcons[${i}])`;
+    }
+}
+
+result;
