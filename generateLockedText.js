368b37e8fab8ac8e6196521118b83f5768caae9a
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-04-21 14:47:46 -0700

    Updating to latest version of OverPy

--- generateLockedText.js
+++ generateLockedText.js
@@ -9,9 +9,9 @@ destroyHudText(zone${pointToLetter[point]}HudText[${i}])`;
     // Create new zone HUD texts
     result += `
 if all([control == Team.1 for control in zoneControl]):
-    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "", "Locked", Position.RIGHT, ${point + 1}, Color.TEAM_1, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)
+    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "", "Locked", HudPosition.RIGHT, ${point + 1}, Color.TEAM_1, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)
 else:
-    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "", "Locked", Position.RIGHT, ${point + 1}, Color.TEAM_2, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)`;
+    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "", "Locked", HudPosition.RIGHT, ${point + 1}, Color.TEAM_2, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)`;
         result += `
 zone${pointToLetter[point]}HudText[0] = getLastCreatedText()
 wait(0.033, Wait.IGNORE_CONDITION)`;
