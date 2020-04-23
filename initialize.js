7c7d9332bb30e93e414855ef3622e8ae6fdbcb41
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-04-23 16:32:48 -0700

    Fix indentation on initialize method (even though it doesn't matter right now)

--- initialize.js
+++ initialize.js
@@ -5,14 +5,14 @@ for (var i = 0;  i < numPoints; i++) {
     // Initialize zone control + flags
     // Generate Zone Progress Headers
     result += `
-    zoneControl[${i}] = null
-    hudHeader([p for p in getAllPlayers() if abs(zone${pointToLetter[i]}Progress) > 0 and not huntActive and distance(vect(p.getPosition().x, zoneLocations[${i}].y, p.getPosition().z), zoneLocations[${i}]) < zoneSizes[${i}] and p.getPosition().y - zoneLocations[${i}].y >= -0.5 and p.getPosition().y - zoneLocations[${i}].y < zoneSizes[${i}]], "Progress: {}%".format(floor(abs(zone${pointToLetter[i]}Progress))), HudPosition.TOP, 2, Color.LIME_GREEN, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)`;
+zoneControl[${i}] = null
+hudHeader([p for p in getAllPlayers() if abs(zone${pointToLetter[i]}Progress) > 0 and not huntActive and distance(vect(p.getPosition().x, zoneLocations[${i}].y, p.getPosition().z), zoneLocations[${i}]) < zoneSizes[${i}] and p.getPosition().y - zoneLocations[${i}].y >= -0.5 and p.getPosition().y - zoneLocations[${i}].y < zoneSizes[${i}]], "Progress: {}%".format(floor(abs(zone${pointToLetter[i]}Progress))), HudPosition.TOP, 2, Color.LIME_GREEN, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)`;
 }
 
 // Generate Zone Separators
 for (var i = 1.5; i < 3; i++) {
     result += `
-    hudSubtext(getAllPlayers(), w"-----------", HudPosition.RIGHT, ${i}, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)`
+hudSubtext(getAllPlayers(), w"-----------", HudPosition.RIGHT, ${i}, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)`
 }
 
 result += `
