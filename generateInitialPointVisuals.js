f2a483aff77eb6bd04c998ebae9babd0fd0f04c2
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-05-27 23:53:41 -0700

    Trial new in-world zone progress format

--- generateInitialPointVisuals.js
+++ generateInitialPointVisuals.js
@@ -17,9 +17,11 @@ var pointToLetter = ['A', 'B', 'C'];
 for (var i = 0; i < pointToLetter.length; i++) {
     conditions.forEach(function (condition) {
         result += `
-        createEffect([p for p in getAllPlayers() if zoneControl[${i}] == ${condition.control}], Effect.RING, Color.${condition.color}, zoneLocations[${i}], zoneSizes[${i}], EffectReeval.VISIBILITY)
-        createInWorldText([p for p in getAllPlayers() if zoneControl[${i}] == ${condition.control} and not powerPlayActive], "${pointToLetter[i]}", zoneLocations[${i}] + 2 * Vector.UP, 3, Clip.NONE, WorldTextReeval.VISIBILITY_AND_STRING, Color.${condition.color}, SpecVisibility.DEFAULT)
-        createInWorldText([p for p in getAllPlayers() if zoneControl[${i}] == ${condition.control} and powerPlayActive], "{0}".format(iconString(Icon.NO)), zoneLocations[${i}] + 2 * Vector.UP, 2, Clip.SURFACES, WorldTextReeval.VISIBILITY, Color.WHITE, SpecVisibility.DEFAULT)`;
+    # Zone ${pointToLetter[i]} Visuals
+    createEffect([p for p in getAllPlayers() if zoneControl[${i}] == ${condition.control}], Effect.RING, Color.${condition.color}, zoneLocations[${i}], zoneSizes[${i}], EffectReeval.VISIBILITY)
+    createInWorldText([p for p in getAllPlayers() if zoneControl[${i}] == ${condition.control} and not powerPlayActive],
+    "${pointToLetter[i]}\r{0}".format(floor(abs(zone${pointToLetter[i]}Progress))), zoneLocations[${i}] + 2 * Vector.UP, 3, Clip.NONE, WorldTextReeval.VISIBILITY_AND_STRING, Color.${condition.color}, SpecVisibility.DEFAULT)
+    createInWorldText([p for p in getAllPlayers() if zoneControl[${i}] == ${condition.control} and powerPlayActive], "{0}".format(iconString(Icon.NO)), zoneLocations[${i}] + 2 * Vector.UP, 2, Clip.SURFACES, WorldTextReeval.VISIBILITY, Color.WHITE, SpecVisibility.DEFAULT)`;
     });
 }
 
