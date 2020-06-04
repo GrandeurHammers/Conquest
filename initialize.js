1fafdbd5f5df6c9b71a556bc92821246d4f6404b
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-06-03 23:15:18 -0700

    - Add changing color to progress bar to better indicate contested status - Alter score multiplier indicator

--- initialize.js
+++ initialize.js
@@ -12,11 +12,17 @@ for (var i = 0; i < numPoints; i++) {
 }
 for (var i = 0;  i < numPoints; i++) {
     // Generate Zone Progress Headers
-    var visTo = `p for p in getLivingPlayers(Team.ALL) if abs(zone${pointToLetter[i]}Progress) > 0 and not powerPlayActive and distance(vect(p.getPosition().x, zoneLocations[${i}].y, p.getPosition().z), zoneLocations[${i}]) < zoneSizes[${i}] and p.getPosition().y - zoneLocations[${i}].y >= -0.5 and p.getPosition().y - zoneLocations[${i}].y < zoneSizes[${i}]`;
+    var visTo = `p for p in getLivingPlayers(Team.ALL) if (abs(zone${pointToLetter[i]}Progress) > 0 and %CONTEST%) and not powerPlayActive and distance(vect(p.getPosition().x, zoneLocations[${i}].y, p.getPosition().z), zoneLocations[${i}]) < zoneSizes[${i}] and p.getPosition().y - zoneLocations[${i}].y >= -0.5 and p.getPosition().y - zoneLocations[${i}].y < zoneSizes[${i}]`;
+    var visToProgress = visTo.replace('%CONTEST%', `not (numTeam1${pointToLetter[i]} > 0 and numTeam2${pointToLetter[i]} > 0)`);
+    var visToContested = visTo.replace('%CONTEST%', `(numTeam1${pointToLetter[i]} > 0 and numTeam2${pointToLetter[i]} > 0)`);
     // Generate Progress Bar
     result += `
-    #Progress Bar for Zone ${pointToLetter[i]} - Visible to all living players within the radius during normal play when point has progress on it
-    ${progressBarHelper(i, visTo)}`;
+    #Progress Bars for Zone ${pointToLetter[i]} - Visible to all living players within the radius during normal play when point has progress on it. First one is for non-contested, second one is for contested.
+    ${progressBarHelper(i, visToProgress, false)}
+    ${progressBarHelper(i, visToContested, true)}`;
+    // Add Capturing/Contested undertext
+    result += `
+    hudSubtext([${visTo.replace('%CONTEST%', 'true')}], "{0} - {1}%".format(zone${pointToLetter[i]}HudText[3], floor(abs(zone${pointToLetter[i]}Progress))), HudPosition.TOP, 5, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)`;
 }
 
 result += `
@@ -34,16 +40,18 @@ result += `
     #Score multiplier under scoreboard
     scoreMultipliers = [1, 1]
     #Team 1 score multiplier
-    hudHeader(getPlayers(Team.1), "{0} | Score Multiplier | {1}".format(scoreMultipliers[0], scoreMultipliers[1]), HudPosition.TOP, 1, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)
+    hudHeader(getPlayers(Team.1), "{0}  |  Multiplier  |  {1}".format(scoreMultipliers[0], scoreMultipliers[1]), HudPosition.TOP, 1, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)
     #Team 2 score multiplier
-    hudHeader(getPlayers(Team.2), "{0} | Score Multiplier | {1}".format(scoreMultipliers[1], scoreMultipliers[0]), HudPosition.TOP, 1, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)
+    hudHeader(getPlayers(Team.2), "{0}  |  Multiplier  |  {1}".format(scoreMultipliers[1], scoreMultipliers[0]), HudPosition.TOP, 1, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)
     #Power Play Timer - Visible when power play is active
     hudHeader([p for p in getAllPlayers() if powerPlayActive], "Power Play: {0}".format(ceil(powerPlayTimer)), HudPosition.TOP, 3, Color.YELLOW, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.DEFAULT)`
 console.log(result);
 result;
 
-function progressBarHelper(i, visTo) {
-    // i = numerical index of zone
+function progressBarHelper(i, visTo, isContested) {
+    // i: numerical index of zone
+    // visTo: visibility conditions
+    // isContested: whether to generate a contested header or not
     var len = 18;
     var emptyChar = "　";
     var fullChar = "▒";
@@ -64,9 +72,6 @@ function progressBarHelper(i, visTo) {
         }
     }
     
-    result += `][round(abs(zone${pointToLetter[i]}Progress) * ${len} / 100)], HudPosition.TOP, 4, Color.LIME_GREEN, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)`;
-    // Add Capturing/Contested undertext and initialize 
-    result += `
-    hudSubtext([${visTo}], "{0} - {1}%".format(zone${pointToLetter[i]}HudText[3], floor(abs(zone${pointToLetter[i]}Progress))), HudPosition.TOP, 5, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)`;
+    result += `][round(abs(zone${pointToLetter[i]}Progress) * ${len} / 100)], HudPosition.TOP, 4, Color.${isContested ? 'YELLOW' : 'LIME_GREEN'}, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)`;
     return result;
 }
