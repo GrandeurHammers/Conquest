bba31e22887aaa5baeb8c1d365d668134efa6ed7
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-06-07 15:01:03 -0700

    Prevent Zone HUD from "bouncing" on updates

--- generateLockedText.js
+++ generateLockedText.js
@@ -1,4 +1,6 @@
 var result = "";
+var emptyChar = '　';
+var progressBarLen = 10; // mirror length in generateVisualUpdates.js
 var pointToLetter = ["A", "B", "C"];
 for (var point = 0; point < 3; point++) {
     // Destroy all zone HUD texts
@@ -10,9 +12,9 @@ destroyHudText(zone${pointToLetter[point]}HudText[${i}])`;
     // Create new zone HUD texts
     result += `
 if all([control == Team.1 for control in zoneControl]):
-    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", " ", "Locked", HudPosition.RIGHT, ${visInd}, Color.TEAM_1, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)
+    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "${emptyChar.repeat(progressBarLen)}", "Locked", HudPosition.RIGHT, ${visInd}, Color.TEAM_1, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)
 else:
-    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", " ", "Locked", HudPosition.RIGHT, ${visInd}, Color.TEAM_2, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)`;
+    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "${emptyChar.repeat(progressBarLen)}", "Locked", HudPosition.RIGHT, ${visInd}, Color.TEAM_2, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)`;
         result += `
 zone${pointToLetter[point]}HudText[0] = getLastCreatedText()`;
 }
