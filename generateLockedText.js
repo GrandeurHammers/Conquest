16e2c2b3e3544d27c33f18d48facd544e9da92e2
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-06-07 23:42:39 -0700

    Fix some minor issues with Power Play: - Add HUD icons to Zone UI to better communicate zones are locked - Ensure Power Play cannot extend beyond time limit - Prevent burning effect from being removed by in-game burn effects

--- generateLockedText.js
+++ generateLockedText.js
@@ -12,9 +12,9 @@ destroyHudText(zone${pointToLetter[point]}HudText[${i}])`;
     // Create new zone HUD texts
     result += `
 if all([control == Team.1 for control in zoneControl]):
-    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "${emptyChar.repeat(progressBarLen)}", "Locked", HudPosition.RIGHT, ${visInd}, Color.TEAM_1, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)
+    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "${emptyChar.repeat(progressBarLen)}", "{0} Locked".format(iconString(Icon.NO)), HudPosition.RIGHT, ${visInd}, Color.TEAM_1, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)
 else:
-    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "${emptyChar.repeat(progressBarLen)}", "Locked", HudPosition.RIGHT, ${visInd}, Color.TEAM_2, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)`;
+    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "${emptyChar.repeat(progressBarLen)}", "{0} Locked".format(iconString(Icon.NO)), HudPosition.RIGHT, ${visInd}, Color.TEAM_2, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.ALWAYS)`;
         result += `
 zone${pointToLetter[point]}HudText[0] = getLastCreatedText()`;
 }
