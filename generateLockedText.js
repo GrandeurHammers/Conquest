719cf5da77c5a99609295eec4ade790275e682c8
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-06-29 21:42:31 -0700

    Ensure flags are shown in zone HUD during Power Play

--- generateLockedText.js
+++ generateLockedText.js
@@ -12,9 +12,9 @@ destroyHudText(zone${pointToLetter[point]}HudText[${i}])`;
     // Create new zone HUD texts
     result += `
 if all([control == Team.1 for control in zoneControl]):
-    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "${emptyChar.repeat(progressBarLen)}", "{0} Locked".format(iconString(Icon.NO)), HudPosition.RIGHT, ${visInd}, Color.TEAM_1, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)
+    hudText(getAllPlayers(), "{0}Zone ${pointToLetter[point]}".format(iconString(Icon.FLAG)), "${emptyChar.repeat(progressBarLen)}", "{0} Locked".format(iconString(Icon.NO)), HudPosition.RIGHT, ${visInd}, Color.TEAM_1, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)
 else:
-    hudText(getAllPlayers(), "Zone ${pointToLetter[point]}", "${emptyChar.repeat(progressBarLen)}", "{0} Locked".format(iconString(Icon.NO)), HudPosition.RIGHT, ${visInd}, Color.TEAM_2, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)`;
+    hudText(getAllPlayers(), "{0}Zone ${pointToLetter[point]}".format(iconString(Icon.FLAG)), "${emptyChar.repeat(progressBarLen)}", "{0} Locked".format(iconString(Icon.NO)), HudPosition.RIGHT, ${visInd}, Color.TEAM_2, Color.WHITE, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)`;
         result += `
 zone${pointToLetter[point]}HudText[0] = getLastCreatedText()`;
 }
