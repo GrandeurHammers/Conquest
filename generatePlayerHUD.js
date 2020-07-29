7617ddb9f882da6765f4b73603c7d818e5b65970
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-07-29 13:54:09 -0700

    Add Input Binding string to respawn HUD

--- generatePlayerHUD.js
+++ generatePlayerHUD.js
@@ -1,5 +1,5 @@
 let spacesAcross = 0;
-var result = `hudHeader([p for p in eventPlayer if p.isDead()], "${"　".repeat(spacesAcross)}Respawning in {0}".format(ceil(eventPlayer.timeToRespawn)) if eventPlayer.respawnBuffered else "${"　".repeat(spacesAcross)}Press Jump to Respawn", `;
+var result = `hudHeader([p for p in eventPlayer if p.isDead()], "${"　".repeat(spacesAcross)}Respawning in {0}".format(ceil(eventPlayer.timeToRespawn)) if eventPlayer.respawnBuffered else "${"　".repeat(spacesAcross)}Press [{0}] to Respawn".format(buttonString(Button.JUMP)), `;
 result += "HudPosition.TOP, 5, Color.YELLOW, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)";
 console.log(result);
 result;
