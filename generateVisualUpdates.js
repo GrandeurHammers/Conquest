e67b8d17701263ff743a0aeb88d95549815ee20e
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-09-25 23:51:50 -0700

    Update generation of constant subtitle HUDs

--- generateVisualUpdates.js
+++ generateVisualUpdates.js
@@ -151,10 +151,11 @@ rule "Zone ${pointToLetter[point]} HUD: Control ${control.zoneControl} | Progres
                 result += `\n\tzone${pointToLetter[point]}HudText[${index}] = getLastCreatedText()`;
             });
         } else if ("subtitleAll" in subtitle) {
+            templateAction = templateAction.replace("%SUBTITLE%", subtitle.subtitleAll);
+
             visKeys.forEach(function (visData, index) {
-                result += `
-    hudText(${visData.players}, "{0}Zone ${pointToLetter[point]}".format(iconString(Icon.FLAG)), ${subtitle.subtitleAll}, ${subtitle[visData.textKey]}, HudPosition.RIGHT, ${visInd}, Color.${control.headerColor}, Color.${subtitle.subtitleColor}, Color.${subtitle.subtextColor}, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)
-    zone${pointToLetter[point]}HudText[${index}] = getLastCreatedText()`;
+                result += "\n\t" + templateAction.replace("%VIS%", visData.players).replace("%SUBTEXT%", subtitle[visData.textKey]).replace("%HUDPOS%", visData.position).replace("%SPECVIS%", visData.specvis);
+                result += `\n\tzone${pointToLetter[point]}HudText[${index}] = getLastCreatedText()`;
             });
         } else {
             visKeys.forEach(function (visData, index) {
