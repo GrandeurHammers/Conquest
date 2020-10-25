7fff765a78d1b16cde341b637078dda46131f91a
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-10-24 21:34:28 -0700

    Set up initial team spawns

--- initialize.js
+++ initialize.js
@@ -5,6 +5,8 @@ const pointToLetter = ['A', 'B', 'C'];
 result += `
     #Set up all zones to have neither team controlling
     zoneControl = [null, null, null]
+    #Start Team 1 at Zone A and Team 2 at Zone C
+    currTeamSpawns = [0,2]
     #Set up all zones to start on non-alt colors
     altColorControl = [false, false, false]
     #Set up all zones to not loop alt colors
