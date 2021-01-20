467eaa2c6b72099a52613e94813c5bc1df63081b
CactusPuppy <puppycactus@gmail.com>
2021-01-19 23:19:55 -0800

    Allow initial visuals to remain permanent through re-eval

--- macros/generateInitialPointVisuals.js
+++ macros/generateInitialPointVisuals.js
@@ -6,16 +6,16 @@ for (var i = 0; i < pointToLetter.length; i++) {
     result += `\n\tcreateEffect(getAllPlayers(), Effect.RING, zone${pointToLetter[i]}MainColor, zoneLocations[${i}], zoneSizes[${i}], EffectReeval.VISIBILITY_AND_COLOR)`;
     // Power play locked icon
     result += `\n\t# Zone ${pointToLetter[i]} Visuals - Locked zone indicator (power play)`;
-    result += `\n\tcreateInWorldText(getAllPlayers() if powerPlayActive else null, "{0}".format(iconString(Icon.NO)), zoneLocations[${i}] + 2 * Vector.UP, 2, Clip.NONE, WorldTextReeval.VISIBILITY, Color.WHITE, SpecVisibility.DEFAULT)`;
+    result += `\n\tcreateInWorldText(getAllPlayers() if powerPlayActive else null, "{0}\r".format(iconString(Icon.NO)), zoneLocations[${i}], 2, Clip.NONE, WorldTextReeval.VISIBILITY, Color.WHITE, SpecVisibility.DEFAULT)`;
     // Normal zone lettering
-    result += `\n\tcreateInWorldText(getAllPlayers() if not powerPlayActive else null, "${pointToLetter[i]}\r", zoneLocations[${i}] + 2 * Vector.UP, 3, Clip.NONE, WorldTextReeval.VISIBILITY_AND_STRING, zoneAMainColor, SpecVisibility.DEFAULT)`;
+    result += `\n\tcreateInWorldText(getAllPlayers() if not powerPlayActive else null, "${pointToLetter[i]}\r\r", zoneLocations[${i}], 3, Clip.NONE, WorldTextReeval.VISIBILITY_AND_STRING, zone${pointToLetter[i]}MainColor, SpecVisibility.DEFAULT)`;
     // Normal percentage indicator
-    result += `\n\tcreateInWorldText(getAllPlayers() if not powerPlayActive else null, " \r{0}%".format(floor(abs(zone${pointToLetter[i]}Progress))) if abs(zone${pointToLetter[i]}Progress) > 0 else " \r ", zoneLocations[${i}] + 2 * Vector.UP, 3, Clip.NONE, WorldTextReeval.VISIBILITY_AND_STRING, zoneAAltColor, SpecVisibility.DEFAULT)`;
+    result += `\n\tcreateInWorldText(getAllPlayers() if not powerPlayActive else null, " \r{0}%\r".format(floor(abs(zone${pointToLetter[i]}Progress))) if abs(zone${pointToLetter[i]}Progress) > 0 else "", zoneLocations[${i}], 3, Clip.NONE, WorldTextReeval.VISIBILITY_AND_STRING, zone${pointToLetter[i]}AltColor, SpecVisibility.DEFAULT)`;
     // Warning signs
     result += `\n\t# Team 1 specific prompts`;
-    result += `\n\tcreateInWorldText(getPlayers(Team.1), "{0}\r\r\r".format(iconString(Icon.WARNING)) if zone${pointToLetter[i]}Progress < 0 and not powerPlayActive else " \r\r\r", zoneLocations[${i}] + 2 * Vector.UP, 2, Clip.NONE, WorldTextReeval.VISIBILITY_AND_STRING, Color.RED, SpecVisibility.NEVER)`;
+    result += `\n\tcreateIcon([p for p in getPlayers(Team.1) if distance(p, zoneLocations[${i}]) > zoneSizes[${i}]] if zone${pointToLetter[i]}Progress < 0 and not powerPlayActive else null, zoneLocations[${i}], Icon.WARNING, IconReeval.VISIBILITY, Color.RED, true)`;
     result += `\n\t# Team 2 specific prompts`;
-    result += `\n\tcreateInWorldText(getPlayers(Team.2), "{0}\r\r\r".format(iconString(Icon.WARNING)) if zone${pointToLetter[i]}Progress > 0 and not powerPlayActive else " \r\r\r", zoneLocations[${i}] + 2 * Vector.UP, 2, Clip.NONE, WorldTextReeval.VISIBILITY_AND_STRING, Color.RED, SpecVisibility.NEVER)`;
+    result += `\n\tcreateIcon([p for p in getPlayers(Team.2) if distance(p, zoneLocations[${i}]) > zoneSizes[${i}]] if zone${pointToLetter[i]}Progress > 0 and not powerPlayActive else null, zoneLocations[${i}], Icon.WARNING, IconReeval.VISIBILITY, Color.RED, true)`;
 }
 console.log(result);
 result;
