f5672c86611c8d6d9537d00617701d661614271b
CactusPuppy <puppycactus@gmail.com>
2021-10-09 22:51:19 -0700

    Fix newline and alignment issues

--- macros/generatePlayerHUDSpacer.js
+++ macros/generatePlayerHUDSpacer.js
@@ -1,8 +1,8 @@
 let linesDown = 10;
 var result = "hudSubtext(getDeadPlayers(Team.ALL), \" ";
-result += "\r".repeat(linesDown - 4);
+result += "\r\n".repeat(linesDown - 4);
 result += "\" if powerPlayActive else \" ";
-result += "\r".repeat(linesDown);
+result += "\r\n".repeat(linesDown);
 result += "\", HudPosition.TOP, 4, Color.WHITE, HudReeval.VISIBILITY_AND_STRING, SpecVisibility.NEVER)"
 console.log(result);
 result;
